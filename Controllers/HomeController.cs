b497a80ee8e38506164e79c62420f6d35831db5d
Mohammad Farazmaan <mohammad.farazmaan@gmail.com>
2020-02-01 21:45:23 +0330

    zarinpal

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,111 @@
+﻿using System.Threading.Tasks;
+using Dto.Other;
+using Dto.Payment;
+using Microsoft.AspNetCore.Mvc;
+using ZarinPal.Class;
+
+namespace ZarinpalTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly Payment _payment;
+        private readonly Authority _authority;
+        private readonly Transactions _transactions;
+
+        public HomeController()
+        {
+            var expose = new Expose();
+            _payment = expose.CreatePayment();
+            _authority = expose.CreateAuthority();
+            _transactions = expose.CreateTransactions();
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// ﻓﺮﺍﻳﻨﺪ ﺧﺮﻳﺪ
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IActionResult> Request()
+        {
+            var result = await _payment.Request(new DtoRequest()
+            {
+                Mobile = "09121112222",
+                CallbackUrl = "https://localhost:44310/home/validate",
+                Description = "توضیحات",
+                Email = "farazmaan@outlook.com",
+                Amount = 1000000,
+                MerchantId = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"
+            }, ZarinPal.Class.Payment.Mode.sandbox);
+            return Redirect($"https://sandbox.zarinpal.com/pg/StartPay/{result.Authority}");
+        }
+
+        /// <summary>
+        /// ﻓﺮﺍﻳﻨﺪ ﺧﺮﻳﺪ ﺑﺎ ﺗﺴﻮﻳﻪ ﺍﺷﺘﺮﺍﻛﻲ 
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IActionResult> RequestWithExtra()
+        {
+            var result = await _payment.Request(new DtoRequestWithExtra()
+            {
+                Mobile = "09121112222",
+                CallbackUrl = "https://localhost:44310/home/validate",
+                Description = "توضیحات",
+                Email = "farazmaan@outlook.com",
+                Amount = 1000000,
+                MerchantId = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX",
+                AdditionalData = "{\"Wages\":{\"zp.1.1\":{\"Amount\":120,\"Description\":\" ﺗﻘﺴﻴﻢ \"}, \" ﺳﻮﺩ ﺗﺮﺍﻛﻨﺶ zp.2.5\":{\"Amount\":60,\"Description\":\" ﻭﺍﺭﻳﺰ \"}}} "
+            }, ZarinPal.Class.Payment.Mode.sandbox);
+            return Redirect($"https://sandbox.zarinpal.com/pg/StartPay/{result.Authority}");
+        }
+        /// <summary>
+        /// اعتبار سنجی خرید
+        /// </summary>
+        /// <param name="authority"></param>
+        /// <param name="status"></param>
+        /// <returns></returns>
+
+        public async Task<IActionResult> Validate(string authority, string status)
+        {
+            var verification = await _payment.Verification(new DtoVerification
+            {
+                Amount = 1000000,
+                MerchantId = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX",
+                Authority = authority
+            }, Payment.Mode.sandbox);
+            return View();
+        }
+
+        /// <summary>
+        /// ﺩﺭ ﺭﻭﺵ ﺍﻳﺠﺎﺩ ﺷﻨﺎﺳﻪ ﭘﺮﺩﺍﺧﺖ ﺑﺎ ﻃﻮﻝ ﻋﻤﺮ ﺑﺎﻻ ﻣﻤﻜﻦ ﺍﺳﺖ ﺣﺎﻟﺘﻲ ﭘﻴﺶ ﺁﻳﺪ ﻛﻪ ﺷﻤﺎ ﺑﻪ ﺗﻤﺪﻳﺪ ﺑﻴﺸﺘﺮ ﻃﻮﻝ ﻋﻤﺮ ﻳﻚ ﺷﻨﺎﺳﻪ ﭘﺮﺩﺍﺧﺖ ﻧﻴﺎﺯ ﺩﺍﺷﺘﻪ ﺑﺎﺷﻴﺪ
+        /// ﺩﺭ ﺍﻳﻦ ﺻﻮﺭﺕ ﻣﻲ ﺗﻮﺍﻧﻴﺪ ﺍﺯ ﻣﺘﺪ زیر ﺍﺳﺘﻔﺎﺩﻩ ﻧﻤﺎﻳﻴﺪ 
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IActionResult> RefreshAuthority()
+        {
+            var refresh = await _authority.Refresh(new DtoRefreshAuthority
+            {
+                Authority = "",
+                ExpireIn = 1,
+                MerchantId = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"
+            }, Payment.Mode.sandbox);
+            return View();
+        }
+
+        /// <summary>
+        /// ﻣﻤﻜﻦ ﺍﺳﺖ ﺷﻤﺎ ﻧﻴﺎﺯ ﺩﺍﺷﺘﻪ ﺑﺎﺷﻴﺪ ﻛﻪ ﻣﺘﻮﺟﻪ ﺷﻮﻳﺪ ﭼﻪ ﭘﺮﺩﺍﺧﺖ ﻫﺎﻱ ﺗﻮﺳﻂ ﻭﺏ ﺳﺮﻭﻳﺲ ﺷﻤﺎ ﺑﻪ ﺩﺭﺳﺘﻲ ﺍﻧﺠﺎﻡ ﺷﺪﻩ ﺍﻣﺎ ﻣﺘﺪ  ﺭﻭﻱ ﺁﻧﻬﺎ ﺍﻋﻤﺎﻝ ﻧﺸﺪﻩ
+        /// ، ﺑﻪ ﻋﺒﺎﺭﺕ ﺩﻳﮕﺮ ﺍﻳﻦ ﻣﺘﺪ ﻟﻴﺴﺖ ﭘﺮﺩﺍﺧﺖ ﻫﺎﻱ ﻣﻮﻓﻘﻲ ﻛﻪ ﺷﻤﺎ ﺁﻧﻬﺎ ﺭﺍ ﺗﺼﺪﻳﻖ ﻧﻜﺮﺩﻩ ﺍﻳﺪ ﺭﺍ ﺑﻪ PaymentVerification ﺷﻤﺎ ﻧﻤﺎﻳﺶ ﻣﻲ ﺩﻫﺪ.
+        /// </summary>
+        /// <returns></returns>
+
+        public async Task<IActionResult> Unverified()
+        {
+            var refresh = await _transactions.GetUnverified(new DtoMerchant
+            {
+                MerchantId = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"
+            }, Payment.Mode.sandbox);
+            return View();
+        }
+    }
+}
